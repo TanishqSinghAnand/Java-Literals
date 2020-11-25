7954f910bed656a4378f156a29603e40cf03a65c
Tanishq0504 <codertanihsq@gmail.com>
2020-11-26 00:38:42 +0530

    Added All Litreal Types

---
+++ src/com/company/Literels.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Literels {
+
+    public static void main(String[] args) {
+        byte numB = 13;
+        System.out.println("Byte : " + numB);
+        int numI = 129;
+        System.out.println("Int : " + numI );
+        short numS = 87;
+        System.out.println("Short : " + numS );
+        long numL = 129555555555555555L;
+        System.out.println("Long : " + numL );
+        char letterA = 'A';
+        System.out.println("Char : " + letterA);
+        float numF = 5.2f;
+        System.out.println("Float : " + numF);
+        double numD = 5.2d;
+        System.out.println("Double : " + numD);
+        boolean bool1 = false ;
+        System.out.println("Boolean : " + bool1);
+        String str1 = "Tanishq" ;
+        System.out.println("String : " + str1);
+
+
+    }
+
+}
